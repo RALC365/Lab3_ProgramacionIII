5a30a10aa6d50ef4fee461a84eb7907308161988
RALC365 <alejandro.carcamo@unitec.edu>
2017-10-27 14:01:50 -0600

    Inicio

---
+++ Lab3_ProgramacionII/src/lab3_programacionii/Personas.java
@@ -0,0 +1,26 @@
+package lab3_programacionii;
+
+public class Personas {
+    private String username;
+    private String password;
+    private String correo_elecronico;
+    private String nombre;
+    private int id;
+    private String fecha_nacimiento;
+
+    public Personas() {
+    }
+
+    public Personas(String username, String password, String correo_elecronico, String nombre, int id, String fecha_nacimiento) {
+        this.username = username;
+        this.password = password;
+        this.correo_elecronico = correo_elecronico;
+        this.nombre = nombre;
+        this.id = id;
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+    
+    
+    
+    
+}
